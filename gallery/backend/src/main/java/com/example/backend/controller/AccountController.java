d33856803cfc589a163d4498d657746de4084041
ryugahee <gagml910@hanmail.net>
2023-11-02 06:11:16 +0900

    z

--- gallery/backend/src/main/java/com/example/backend/controller/AccountController.java
+++ gallery/backend/src/main/java/com/example/backend/controller/AccountController.java
@@ -4,11 +4,16 @@ import com.example.backend.entity.Item;
 import com.example.backend.entity.Member;
 import com.example.backend.repository.ItemRepository;
 import com.example.backend.repository.MemberRepository;
+import com.example.backend.service.JwtService;
+import com.example.backend.service.JwtServiceImpl;
+import io.jsonwebtoken.Claims;
+import jakarta.servlet.http.Cookie;
+import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.util.List;
 import java.util.Map;
@@ -19,15 +24,53 @@ public class AccountController {
     @Autowired
     MemberRepository memberRepository;
 
+    @Autowired
+    JwtService jwtService;
+
     @PostMapping("/api/account/login")
-    public int login(
-            @RequestBody Map<String, String> params) {  // find메소드의 인자를 'params'라는 이름으로 받음
+    public ResponseEntity login(
+            @RequestBody Map<String, String> params,  // find메소드의 인자를 'params'라는 이름으로 받음
+            HttpServletResponse res) {
         Member member = memberRepository.findByEmailAndPassword(params.get("email"), params.get("password"));
 
         if (member != null) {
-            return member.getId();
+            JwtService jwtService = new JwtServiceImpl();
+            int id = member.getId();
+            // 아이디를 토큰화 함
+            String token = jwtService.getToken("id", id);
+
+            // 토큰을 쿠키에 넣어줌
+            Cookie cookie = new Cookie("token", token);
+            cookie.setHttpOnly(true);
+            cookie.setPath("/");
+
+            res.addCookie(cookie);
+
+            return new ResponseEntity<>(id, HttpStatus.OK);
         }
-        return 0;
+        throw new ResponseStatusException(HttpStatus.NOT_FOUND);
+    }
+
+    @PostMapping("/api/account/logout")
+    public ResponseEntity logout(HttpServletResponse res) {
+        Cookie cookie = new Cookie("token", null);
+        cookie.setPath("/");
+        cookie.setMaxAge(0);
+
+        res.addCookie(cookie);
+        return new ResponseEntity<>(HttpStatus.OK);
+    }
+
+    @GetMapping("/api/account/check")
+    public ResponseEntity check(@CookieValue(value = "token", required = false) String token) {
+        Claims claims = jwtService.getClaims(token);
+
+        if (claims != null) {
+            int id = Integer.parseInt(claims.get("id").toString());
+            return new ResponseEntity<>(id, HttpStatus.OK);
+        }
+
+        return new ResponseEntity<>(null, HttpStatus.OK);
     }
 
 }
